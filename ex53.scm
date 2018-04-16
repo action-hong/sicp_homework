169f46031fdd629169fd52b93bc833a257423653
action-hong <kkopitehong@gmail.com>
2018-04-16 17:44:59 +0800

    w

---
+++ ex53.scm
@@ -0,0 +1,34 @@
+; `()
+
+; `a
+
+; (define a 1)
+
+; a
+
+(define (memq item x)
+  (cond ((null? x) #f)
+        ((eq? item (car x)) x)
+        (else (memq item (cdr x)))
+        ))
+
+(list `a `b `c)
+; (a b c)
+
+(list (list `george))
+; ((george))
+
+(cdr `((x1 x2) (y1 y2)))
+; ((y1 y2))
+
+(cadr `((x1 x2) (y1 y2)))
+; (y1 y2)
+
+(pair? (car `(a shot list)))
+; false
+
+(memq `red `((red shoes) (blue socks)))
+; false
+
+(memq `red `(red shoes blue socks))
+; (red shoes blue socks)
